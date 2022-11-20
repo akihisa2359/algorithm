2eba2626b17a6a88861921ad7194f2906994eec7
akihisa.makimoto <akihisa.makimoto@akihisamakimotonoMacBook-Air.local>
2022-11-20 18:53:57 +0900

    bubble sort 2

---
+++ aizu/ALDS1/2_A_2.py
@@ -0,0 +1,12 @@
+N = int(input())
+*A, = map(int, input().split())
+
+count = 0
+for i in range(N - 1):
+  for j in range(N - i - 1):
+    if (A[j] > A[j + 1]):
+      A[j], A[j + 1] = A[j + 1], A[j]
+      count += 1
+
+print(*A)
+print(count)
