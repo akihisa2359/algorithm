ee59b58417a77613080254316183fe2b41b94439
akihisa.makimoto <akihisa2359@gmail.com>
2022-11-21 23:58:41 +0900

    ExhaustiveSearch2

---
+++ aizu/ALDS1/5_A_2.py
@@ -0,0 +1,18 @@
+N = int(input())
+*choices, = map(int, input().split())
+O = int(input())
+*A, = map(int, input().split())
+
+def combo(i, m):
+  if (m == 0):
+    return True
+  if (m < 0 or i == N):
+    return False
+  
+  return combo(i + 1, m) or combo(i + 1, m - A[i])
+
+for i in range(O):
+  if combo(0, A[i]):
+    print("yes")
+  else:
+    print('no')
