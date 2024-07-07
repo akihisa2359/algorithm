89fdaa607d900b4892e4abaca007b09ef7525e07
akihisa.makimoto <akihisa2359@gmail.com>
2024-07-07 15:28:21 +0900

    値渡しver

--- aizu/ALDS1/5_B.py
+++ aizu/ALDS1/5_B.py
@@ -1,33 +1,34 @@
 def merge_sort(arr, counter):
   if len(arr) == 1:
-    return
+    return arr
+  mid = len(arr) // 2
 
-  arr_l = arr[:(len(arr) // 2)]
-  arr_r = arr[(len(arr) // 2):]
-
-  merge_sort(arr_l, counter)
-  merge_sort(arr_r, counter)
+  arr_l = merge_sort(arr[:mid], counter)
+  arr_r = merge_sort(arr[mid:], counter)
 
   arr_l.append(float('inf'))
   arr_r.append(float('inf'))
 
   i = 0
   j = 0
+  sorted_arr = []
 
   for k in range(len(arr)): # infを足した分2引く
     counter[0] += 1
     if arr_l[i] < arr_r[j]:
-      arr[k] = arr_l[i]
+      sorted_arr.append(arr_l[i])
       i += 1
     else:
-      arr[k] = arr_r[j]
+      sorted_arr.append(arr_r[j])
       j += 1
+  
+  return sorted_arr
 
 n = int(input())
 ls = list(map(int, input().split()))
 
 # ls = [6,3,9,2,1,10]
 counter = [0]
-merge_sort(ls, counter)
-print(*ls)
+res = merge_sort(ls, counter)
+print(*res)
 print(counter[0])
